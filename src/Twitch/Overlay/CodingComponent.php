8dab8937002fb1b1811fb8b23d78e1a39437977b
TBoileau <t-boileau@email.com>
2023-11-12 21:45:00 +0100

    refactor: ajout d'une classe abstraite pour les composants de type overlay

--- src/Twitch/Overlay/CodingComponent.php
+++ src/Twitch/Overlay/CodingComponent.php
@@ -4,25 +4,9 @@ declare(strict_types=1);
 
 namespace App\Twitch\Overlay;
 
-use App\Twitch\TwitchProviderInterface;
-use Symfony\Component\PropertyAccess\PropertyAccess;
 use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
 
-/**
- * @method getLastSubscriber(): string
- * @method getLastFollower(): string
- * @method getTopCheers(): string
- */
 #[AsTwigComponent('twitch_overlay_coding', template: 'twitch/overlays/coding.html.twig')]
-final class CodingComponent
+final class CodingComponent extends OverlayComponent
 {
-    public function __construct(private readonly TwitchProviderInterface $twitchProvider)
-    {
-    }
-
-    public function __call(string $name, array $args): string
-    {
-        $propertyAccessor = PropertyAccess::createPropertyAccessor();
-        return $propertyAccessor->getValue($this->twitchProvider, $name);
-    }
 }
--- src/Twitch/Overlay/JustChattingComponent.php
+++ src/Twitch/Overlay/JustChattingComponent.php
@@ -4,25 +4,9 @@ declare(strict_types=1);
 
 namespace App\Twitch\Overlay;
 
-use App\Twitch\TwitchProviderInterface;
-use Symfony\Component\PropertyAccess\PropertyAccess;
 use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
 
-/**
- * @method getLastSubscriber(): string
- * @method getLastFollower(): string
- * @method getTopCheers(): string
- */
 #[AsTwigComponent('twitch_overlay_just_chatting', template: 'twitch/overlays/just_chatting.html.twig')]
-final class JustChattingComponent
+final class JustChattingComponent extends OverlayComponent
 {
-    public function __construct(private readonly TwitchProviderInterface $twitchProvider)
-    {
-    }
-
-    public function __call(string $name, array $args): string
-    {
-        $propertyAccessor = PropertyAccess::createPropertyAccessor();
-        return $propertyAccessor->getValue($this->twitchProvider, $name);
-    }
 }
