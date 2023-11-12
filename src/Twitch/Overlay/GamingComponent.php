9808bfefcc5daa08d9ccfe80535859a9662e2557
TBoileau <t-boileau@email.com>
2023-11-12 22:26:38 +0100

    feat (#9): overlay Gaming

---
+++ src/Twitch/Overlay/GamingComponent.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Twitch\Overlay;
+
+use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
+
+#[AsTwigComponent('twitch_overlay_gaming', template: 'twitch/overlays/gaming.html.twig')]
+final class GamingComponent extends OverlayComponent
+{
+}
