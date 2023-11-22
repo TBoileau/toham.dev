049018670dba4f982f0e9a3ff8a2190b310cad8c
TBoileau <t-boileau@email.com>
2023-11-22 14:44:06 +0100

    feat: create the ovrlay page to render a specific overlay with its params

--- src/Controller/TwitchController.php
+++ src/Controller/TwitchController.php
@@ -9,12 +9,22 @@ use App\Twitch\OAuth\TwitchAuthenticatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Attribute\MapQueryString;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[Route('/twitch', name: 'twitch_')]
 final class TwitchController extends AbstractController
 {
+    #[Route('/overlays/{name}', name: 'overlay', methods: [Request::METHOD_GET])]
+    public function overlays(string $name, Request $request): Response
+    {
+        return $this->render('twitch/overlay.html.twig', [
+            'name' => $name,
+            'params' => $request->query->all(),
+        ]);
+    }
+
     #[Route('/get-authorization', name: 'get_authorization', methods: [Request::METHOD_GET])]
     public function getAuthorization(Request $request, TwitchAuthenticatorInterface $twitchAuthenticator): RedirectResponse
     {
